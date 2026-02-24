80e7f047784d8bd47c2553313e7f858929200fac
lachiem1 <lachiem1@gmail.com>
2026-02-24 21:58:02 +1100

    Revert "feat: add config and transactions sync with updated TUI views"
    This reverts commit 96c067cd03c0d1c44e6053da07a209f71749d8c4.

--- internal/syncer/service.go
+++ internal/syncer/service.go
@@ -21,11 +21,3 @@ func (s *Service) LeaveView() {
 func (s *Service) RefreshAccounts() error {
 	return s.engine.ManualRefresh(CollectionAccounts)
 }
-
-func (s *Service) EnterTransactionsView(ctx context.Context) error {
-	return s.engine.EnterView(ctx, CollectionTransactions)
-}
-
-func (s *Service) RefreshTransactions() error {
-	return s.engine.ManualRefresh(CollectionTransactions)
-}
